32fd336bffb3978db25697a650a7cb715a87cf77
Alex Matveyev <amatveye@redhat.com>
2019-02-10 18:03:11 -0500

    fix

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <title>Welcome to OpenShift</title>
+</head>
+<body>
+    <p>To test the database, hit the Test DB</p>
+    <p>This assumes that you have the correct env variables set</p>
+    <p>The environment variables required are databaseuser, databasepassword, and databasenmae.</p>
+    <p>---</p>
+    <a href="dbtest.php">Test DB</a>
+    <p>---</p>
+    <a href="create-mysql-table.php">Create MariaDB table</a>
+    <a href="add-record-form.php">Add records to DB</a>
+    <a href="index.php">Index Page</a>
 
-echo "Welcome to OpenShift Online Developer Preview";
-echo "<br>To test the database, hit the dbtest.php URL";
-
-
-echo "<br><br>This assumes that you have the correct env variables set";
-echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
-
-<a href="dbtest.php">Test DB connection</a>
-<a href="create-mysql-table.php">Create MariaDB table</a>
-<a href="add-record-form.php">Add records to DB</a>
-<a href="index.php">Index Page</a>
+    <!--Text in Strong-->
+    <p><strong>OpenShift</strong></p>
+</body>
+</html>
